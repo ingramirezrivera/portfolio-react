d55c4be0201592490ed71e26977e7e8511f58618
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-09-06 10:55:00 -0500

    Deployed and hosting with firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-TXzSYruYHRk92fwBhWv6qv8ZSEfh_S4",
+  authDomain: "portfolio-react-6a0ea.firebaseapp.com",
+  projectId: "portfolio-react-6a0ea",
+  storageBucket: "portfolio-react-6a0ea.appspot.com",
+  messagingSenderId: "108205985628",
+  appId: "1:108205985628:web:34b838238f7cc23f9cbb99"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
