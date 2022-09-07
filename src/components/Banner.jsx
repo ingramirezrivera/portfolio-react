1badae5c83c9dc6333573da724b30990c3377bfc
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-09-07 10:00:21 -0500

    Adding button download CV

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
-import { BsArrowRightCircle } from "react-icons/bs";
+import { BsArrowRightCircle, BsDownload } from "react-icons/bs";
 import Button from "react-bootstrap/Button";
 
 const Banner = () => {
@@ -22,7 +22,7 @@ const Banner = () => {
                 on. I like to work as a team to do great things and enjoy the
                 process...
               </p>
-              <button className="vvd">
+              <button className="vvd" id="button-banner">
                 <span>
                   <a href="#connect">
                     Let’s Connect <BsArrowRightCircle />
@@ -31,13 +31,16 @@ const Banner = () => {
               </button>
             </div>
           </Col>
-          <Col xs={12} md={6} xl={5} className="header-img">
+          <Col xs={12} md={6} xl={5} className="header-img d-flex justify-content-center flex-column">
             <img
               src={
                 "https://firebasestorage.googleapis.com/v0/b/portfolio-react-6a0ea.appspot.com/o/assets%2Fimages%2Fperfil_daniel_ramirez_rivera.png?alt=media&token=ea66b280-97a1-4e5d-ac2b-0791118d771f"
               }
-              alt="Daniel Ramírez image"
+              alt="Daniel Ramírez"
             />
+            <span>
+              <button className="button-download"> Download CV <BsDownload /></button>
+            </span>
           </Col>
         </Row>
       </Container>
