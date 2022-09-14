925e9a778989b8fa62ecfcf44a6ca6dd9fbc9a10
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-09-14 09:57:25 -0500

    Adding contact form to db firestore

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 import 'animate.css';
 import { useState } from "react";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore"; 
+
 
 export const Contact = () => {
   const formInitialDetails = {
@@ -18,10 +21,24 @@ export const Contact = () => {
       ...formDetails,
       [category]: value
     })
-}
+  }
 
-  const handleSubmit = () => {
-    console.log("submited")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("submitted");
+    try {
+      const docRef = await addDoc(collection(db, "contacts"), {
+        firstName: formDetails.firstName,
+        lastName: formDetails.lastName,
+        email: formDetails.email,
+        phone: formDetails.phone,
+        message: formDetails.message,
+      });
+      console.log("Document written with ID: ", docRef.id);
+      alert('Thanks! The Message has been submitted.');
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   }
 
   return (
@@ -46,10 +63,10 @@ export const Contact = () => {
                       <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.message} placeholder="Phone No." onChange={(e) => onFormUpdate('message', e.target.value)}/>
+                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
                     </Col>
                     <Col size={12} sm={6} md={12} className="px-1">
-                      <textarea rows="6" value='' placeholder="Message" onChange=''></textarea>
+                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                       <button type="submit"><span>Submit</span></button>
                     </Col>
                     
